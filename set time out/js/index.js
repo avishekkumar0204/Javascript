3118dcd30684d6efd968eb308aaab9d4229ec9bd
Avishek kumar <46109905+avishekkumar0204@users.noreply.github.com>
2021-04-26 00:02:10 +0530

    Add files via upload

--- set time out/js/index.js
+++ set time out/js/index.js
@@ -1,8 +1,13 @@
 const coupon = document.getElementById("coupon");
-        let yourCoupon = () => {
-        coupon.innerHTML = "Loading....";
-        setTimeout(printCoupon = () => {
-                coupon.innerHTML = "Use <b>AVISHEK1998</b> to get 75% discount";
-        }, 2000)
+let yourCoupon = () => {
+    coupon.innerHTML = "Loading....";
+    let myVar=setTimeout(printCoupon = () => {
+        coupon.innerHTML = "Use <b>AVISHEK1998</b> to get 75% discount";
+    }, 2000)
+    return myVar;
 
+}
+let noNeed=(myVar)=>{
+    clearTimeout(myVar);
+    coupon.innerHTML = "Thank you";
 }
