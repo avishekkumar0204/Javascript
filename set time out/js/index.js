67df7709a1d67e005e2bcc63a1b89583fdc13cef
Avishek kumar <46109905+avishekkumar0204@users.noreply.github.com>
2021-04-25 23:26:43 +0530

    Add files via upload

---
+++ set time out/js/index.js
@@ -0,0 +1,8 @@
+const coupon = document.getElementById("coupon");
+        let yourCoupon = () => {
+        coupon.innerHTML = "Loading....";
+        setTimeout(printCoupon = () => {
+                coupon.innerHTML = "Use <b>AVISHEK1998</b> to get 75% discount";
+        }, 2000)
+
+}
