6e413adf64a5701108c2d304a0be5ab15c63ce25
Avishek <kumaravishek84@gmail.com>
2021-05-31 23:01:34 +0530

    added event

--- Events/events.js
+++ Events/events.js
@@ -1,8 +1,13 @@
 function sayHello(){
     alert("Hello");
 }
-let hiBtn=document.getElementById("hiBtn");
-hiBtn.addEventListener("click",sayHi);
 function sayHi(){
     alert("Hi");
 }
+function sayHiiiiiiiii(){
+    alert("Hiiiiiiiii");
+}
+// Using eventlistner we can add more than one events
+let hiBtn=document.getElementById("hiBtn");
+hiBtn.addEventListener("click",sayHi);
+hiBtn.addEventListener("click",sayHiiiiiiiii);
